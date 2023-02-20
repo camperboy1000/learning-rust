13380dd1b2f30b57f9e047a2bf29c14ce63fa261
Jacob Highfield <camperboy1000@gmail.com>
2023-02-19 23:52:57 -0500

    Print errors to stderr

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,12 +5,12 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("IO Error: {}", e);
+        eprintln!("IO Error: {}", e);
         process::exit(1);
     }
 }
