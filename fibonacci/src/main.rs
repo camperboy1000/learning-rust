f97dd74c622a186a6e7774db4751fd6c3afa51e9
Jacob Highfield <camperboy1000@gmail.com>
2023-03-18 00:39:59 -0400

    Add cache to fibonacci function for SPEED

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,19 +1,46 @@
 use std::cmp::Ordering;
+use std::collections::BTreeMap;
 use std::io;
 use std::io::Write;
+use std::ops::Sub;
+use std::sync::Mutex;
 
-fn fibonacci(n: u32) -> u128 {
+static FIBONACCI_MAP: Mutex<BTreeMap<u128, u128>> = Mutex::new(BTreeMap::new());
+
+fn fibonacci(n: &u128) -> u128 {
     match n.cmp(&2) {
         Ordering::Less => 0,
         Ordering::Equal => 1,
-        Ordering::Greater => fibonacci(n - 1) + fibonacci(n - 2),
+        Ordering::Greater => {
+            let fibonacci_map = FIBONACCI_MAP
+                .lock()
+                .expect("Locking thread paniced when reading value!");
+
+            match fibonacci_map.get(n) {
+                Some(result) => result.clone(),
+                None => {
+                    drop(fibonacci_map);
+
+                    let result1 = fibonacci(&n.sub(1));
+                    let result2 = fibonacci(&n.sub(2));
+
+                    let final_result = result1 + result2;
+                    let mut fibonacci_map = FIBONACCI_MAP
+                        .lock()
+                        .expect("Locking thread paniced when setting value!");
+                    fibonacci_map.insert(n.clone(), final_result.clone());
+
+                    final_result
+                }
+            }
+        }
     }
 }
 
 fn main() {
     println!("Let's calculate the Fibonacci series!");
 
-    let n: u32 = loop {
+    let n: u128 = loop {
         print!("Provide the nth term to calculate: ");
         io::stdout().flush().expect("Failed to flush stdout!");
 
@@ -29,5 +56,5 @@ fn main() {
         };
     };
 
-    println!("Result: {}", fibonacci(n));
+    println!("Result: {}", fibonacci(&n));
 }
